52666be055f50b12ad2eb08ecf56b15f43ff5c26
dongyudadiguo <3662728251@qq.com>
2026-06-01 18:12:20 +0800

    Update [[path]].js

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,9 +1,8 @@
-const ORIGIN = "http://124.221.146.23:8080";
+const ORIGIN = "http://124-221-146-23.sslip.io:8080";
 
 export async function onRequest(context) {
   const incomingUrl = new URL(context.request.url);
 
-  // 调试接口：确认 Function 是否真的部署成功
   if (incomingUrl.pathname === "/api/__debug") {
     return Response.json({
       ok: true,
@@ -21,15 +20,7 @@ export async function onRequest(context) {
     );
 
     const headers = new Headers(context.request.headers);
-
-    // 这些头不要转发给你的 Go 后端
     headers.delete("host");
-    headers.delete("cf-connecting-ip");
-    headers.delete("cf-ipcountry");
-    headers.delete("cf-ray");
-    headers.delete("cf-visitor");
-    headers.delete("x-forwarded-proto");
-    headers.delete("x-real-ip");
 
     const init = {
       method: context.request.method,
@@ -37,16 +28,12 @@ export async function onRequest(context) {
       redirect: "manual",
     };
 
-    if (
-      context.request.method !== "GET" &&
-      context.request.method !== "HEAD"
-    ) {
+    if (context.request.method !== "GET" && context.request.method !== "HEAD") {
       init.body = context.request.body;
     }
 
     const resp = await fetch(targetUrl.toString(), init);
 
-    // 原样返回后端响应
     return new Response(resp.body, {
       status: resp.status,
       statusText: resp.statusText,

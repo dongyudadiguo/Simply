56b37ad96dd198a4e86333e68bb3fcf5f7a3a16e
dongyudadiguo <3662728251@qq.com>
2026-08-10 09:02:52 +0800

    add 插件：栈顶两个 u32 相加覆盖写回，栈回退 4，大小区记录 4（C 语义：stk[0]=stk[0]+stk[4]）

---
+++ 7e9e5ac30f2216fd0fd6f5faed316f2d5983361a4203c3330cfa46ef65bb4767.py
@@ -0,0 +1,12 @@
+# add 插件：栈顶两个 u32 相加，结果覆盖写回，大小区记录 4
+# C 语义：*(u32*)stk = *(u32*)stk + *(u32*)(stk+4); n=4; *(u32*)num = n
+import struct
+import vmstate
+
+def run(payload=""):
+    o = vmstate.stk_off
+    a = struct.unpack_from("<I", vmstate.stk, o-8)[0]
+    b = struct.unpack_from("<I", vmstate.stk, o-4)[0]
+    struct.pack_into("<I", vmstate.stk, o-8, a + b)   # stk[栈顶] = a+b
+    vmstate.stk_off = o - 4                            # 两个变一个，栈回退 4
+    vmstate.write_num(4)                               # num 区记录结果大小
